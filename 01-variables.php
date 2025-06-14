910da0177dfaf4fcc63957f19172c1714776e99d
Felipe Coll <leonfelipecoll@gmail.com>
2025-06-14 20:57:16 -0300

    Fundamentos de php

---
+++ 01-variables.php
@@ -0,0 +1,16 @@
+<?php 
+    $course = "Curso profesional de PHP"
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><? $course ?></title>
+</head>
+<body>
+    <h1></h1><?php echo $course; ?></h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae sapiente aut, facilis, ab perferendis maiores reiciendis vero adipisci commodi quam saepe dolores voluptates, ullam id rem laudantium! Sunt, provident quas.</p>
+</body>
+</html>
