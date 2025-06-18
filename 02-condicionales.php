dfd21d292e62dc07c23bf59baa39f621d681d5fa
Felipe Coll <leonfelipecoll@gmail.com>
2025-06-18 20:49:39 -0300

    Fundamentos de php - correciones de clase

---
+++ 02-condicionales.php
@@ -0,0 +1,24 @@
+<?php 
+    $course = "Curso profesional de PHP"
+    $archive = true
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><? $course ?></title>
+</head>
+<body>
+    <h1></h1><?php echo $course; ?></h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae sapiente aut, facilis, ab perferendis maiores reiciendis vero adipisci commodi quam saepe dolores voluptates, ullam id rem laudantium! Sunt, provident quas.</p>
+
+    <?php ?>
+        if ($archive) {
+            echo "<p>El curso está archivado.</p>";
+        } else {
+            echo "<p>El curso no está archivado.</p>";
+        }
+</body>
+</html>
