c09f9a022b2f7cf2b508ce1cb39d27a7ef88b735
Felipe Coll <leonfelipecoll@gmail.com>
2025-06-23 15:15:29 -0300

    Fundamentos de php - condicionales - finalizado

--- 02-condicionales.php
+++ 02-condicionales.php
@@ -1,25 +1,40 @@
-<?php 
-    $course = "Curso profesional de PHP"
-    $archive = false
-    
-?>
+<?php
+$curso = "Curso Profesional  PHP y laravel ";
+$costo ="Gratis";
+$fecha =date("Y-m-d");
+$archivado = true;
+$status = $archivado ? "Archivado" : "Activo"; //operador ternario
+$nivel =  "Avanzado";
 
+switch($nivel){
+case "Basico":
+    $nivelDesc="Personas con conocimientos basicos de programacion";
+    break;
+    case "Intermedio":
+       $nivelDesc= "Personas con conocimentos previos de progrmacion y base de datos";
+        break;
+        case "Avanzado":
+            $nivelDesc= "Personas con amplios conocimientos en la materia ";
+            break;
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><? $course ?></title>
+    <title><?= $curso ?></title>
 </head>
 <body>
-    <h1></h1><?php echo $course; ?></h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae sapiente aut, facilis, ab perferendis maiores reiciendis vero adipisci commodi quam saepe dolores voluptates, ullam id rem laudantium! Sunt, provident quas.</p>
 
-    <?php ?>
-        if ($archive) {
-            echo "<p>El curso está archivado.</p>";
-        } else {
-            echo "<p>El curso no está archivado.</p>";
-        }
+     
+     
+     
+    <h2>Bienvenido a el <?=$curso?></h2>
+    <h3>Costo: <?= $costo ?></h3>
+    <h3>Publicado: <?= $fecha?></h3>
+       <p>El curso esta <?= $status ?></p>
+    <p>Este curso esta diseñado para <?= $nivelDesc ?></p>
+ 
 </body>
 </html>
